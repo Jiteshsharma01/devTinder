a22669f2df70d663b0851ca8311f9c89cbdcd06b
Jitesh01 <jitesh171099@gmail.com>
2025-02-10 21:53:13 +0530

    url added

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 
 app.use("/test",(req, res) => {
-    res.send("hello from server!");
-})
+    res.send("hello from test!");
+});
+app.use("/",(req, res) => {
+    res.send("hello from home!");
+});
 
 app.listen(3000, () => {
     console.log("listening on port 3000...");
