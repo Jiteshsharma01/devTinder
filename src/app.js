b4e1dfc55c6febb6edf04aeadac0caf11a434e86
Jitesh01 <jitesh171099@gmail.com>
2025-02-20 22:41:53 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,15 +1,30 @@
-const express = require('express');
-
+const express = require("express");
+const User = require("./models/user");
+const connectDB = require("./config/database");
 const app = express();
 
-app.use("/test",(req, res) => {
-    res.send("hello from test!");
-});
-app.use("/",(req, res) => {
-    res.send("hello from home!");
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Rohit",
+    lastName: "Sharma",
+    emailId: "rohit@sharma.com",
+    password: "rohit@123",
+  });
+  try {
+    await user.save();
+    res.send("User added successfully!");
+  } catch (error) {
+    res.status(404).send("Error creating user" + error.message);
+  }
 });
 
-app.listen(3000, () => {
-    console.log("listening on port 3000...");
-    
-});
+connectDB()
+  .then(() => {
+    console.log("database connection established...");
+    app.listen(7777, () => {
+      console.log("listening on port 7777...");
+    });
+  })
+  .catch((err) => {
+    console.error("connection error");
+  });
